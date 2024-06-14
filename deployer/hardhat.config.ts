c30410aa7e97f2f8db05d49a01467260f438aff3
Qwertvz <stalker-stalkerovich@list.ru>
2024-06-14 23:20:55 +0700

    Updated config

--- deployer/hardhat.config.ts
+++ deployer/hardhat.config.ts
@@ -3,6 +3,7 @@ import "@nomicfoundation/hardhat-toolbox"
 
 import * as dotenv from "dotenv"
 
+
 dotenv.config()
 
 const config: HardhatUserConfig = {
@@ -17,9 +18,25 @@ const config: HardhatUserConfig = {
     defaultNetwork: "hardhat",
     networks: {
         localnet: {
-            url: `http://${process.env.INTERNAL_NODE_HOST}:${process.env.INTERNAL_NODE_PORT}`
+            url: `http://${process.env.INTERNAL_NODE_HOST}:${process.env.INTERNAL_NODE_PORT}`,
+            chainId: Number(process.env.NODE_CHAIN_ID)
         }
-    }
+    },
+
+    etherscan: {
+        apiKey: {
+            localnet: "IS_NOT_REQUIRED_FOR_LOCAL_BLOCKSCOUT"
+        },
+        customChains: [{
+            network: "localnet",
+            chainId: Number(process.env.NODE_CHAIN_ID),
+            urls: {
+                apiURL: String(process.env.URL_EXPLORER_API),
+                browserURL: String(process.env.URL_EXPLORER)
+            }
+        }]
+    },
+    sourcify: { enabled: false }
 }
 
 
