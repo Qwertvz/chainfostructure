852fb839e7033200ac7773f9f979f42ea3a11c78
Qwertvz <stalker-stalkerovich@list.ru>
2024-06-14 23:21:20 +0700

    Added contract and deploy script

--- deployer/scripts/deploy.ts
+++ deployer/scripts/deploy.ts
@@ -1,7 +1,8 @@
 import { ethers } from "hardhat"
 import type { ERC20Token } from "../typechain-types"
+import type { CryptonPass } from "../typechain-types"
 
-async function main() {
+async function DeployERC20Token() {
     const [deployer] = await ethers.getSigners()
     console.log("Deploying contracts with the account:", deployer.address)
 
@@ -9,6 +10,21 @@ async function main() {
     const ERC20Token: ERC20Token = await ERC20TokenCode.deploy("Tether", "USDT", deployer.address)
 
     console.log("Token deployed to:", await ERC20Token.getAddress())
+}
+
+async function DeployCryptonPass() {
+    const [deployer] = await ethers.getSigners()
+    console.log("Deploying contracts with the account:", deployer.address)
+    
+    const CryptonPassCode = await ethers.getContractFactory("CryptonPass")
+    const CryptonPass: CryptonPass = await CryptonPassCode.deploy(deployer.address)
+    
+    console.log("Token deployed to:", await CryptonPass.getAddress())
+}
+
+async function main() {
+    DeployERC20Token()
+    DeployCryptonPass()
 }
 
 main().then(
